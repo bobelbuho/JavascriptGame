e3b173a116473bcf549d94042398f66d0754e206
Bob <bob.vaz@hotmail.fr>
2024-05-21 00:26:58 +0200

    fix bug acces token

--- Documents/projects/docblog/frontend/src/components/ProtectedRoute.jsx
+++ Documents/projects/docblog/frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
 import {Navigate} from "react-router-dom"
 import {jwtDecode} from "jwt-decode"
 import api from "../api"
-import { REFRESH_TOKEN, ACCES_TOKEN } from "../constants"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
 import { useState, useEffect } from "react"
-import { set } from "express/lib/response"
 
 
 function ProtectedRoute({children}){
@@ -21,7 +20,7 @@ function ProtectedRoute({children}){
                 refresh: refreshToken,
             });
             if (res.status === 200){
-                localStorage.setItem(ACCES_TOKEN, res.data.access)
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
                 setIsAuthorized(true)
             } else {
                 setIsAuthorized(false)
@@ -32,7 +31,7 @@ function ProtectedRoute({children}){
         }
     }
     const auth = async () => {
-        const token = localStorage.getItem(ACCES_TOKEN)
+        const token = localStorage.getItem(ACCESS_TOKEN)
         if(!token){
             setIsAuthorized(false)
             return
