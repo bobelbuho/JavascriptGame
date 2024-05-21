452b256a1804135c7da740f94ff9861d99f0b0ad
Bob <bob.vaz@hotmail.fr>
2024-05-22 00:03:49 +0200

    1h32

--- Documents/projects/docblog/frontend/src/pages/Home.jsx
+++ Documents/projects/docblog/frontend/src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import {useState, useEffect} from 'react'
+import api from "../api"
+
 function Home() {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+    
     return <h1>Home</h1>
 }
 export default Home
+
