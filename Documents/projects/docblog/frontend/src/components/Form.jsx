751700ea47866dd43147018314e392f74ef818f0
Bob <bob.vaz@hotmail.fr>
2024-05-21 23:54:14 +0200

    bug login register

--- Documents/projects/docblog/frontend/src/components/Form.jsx
+++ Documents/projects/docblog/frontend/src/components/Form.jsx
@@ -3,21 +3,22 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN } from "../constants";
 import "../styles/Form.css" 
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-    const name = method === "login" ? "Login" : "Register"
+    const name = method === "login" ? "Login" : "Register";
 
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
 
         try { 
-            const res = await api.post(route, {username, password})
+            const res = await api.post(route, { username, password })
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.acess);
                 localStorage.setItem(ACCESS_TOKEN, res.data.refresh);
@@ -31,9 +32,10 @@ function Form({ route, method }) {
         } finally {
             setLoading(false)
         }
-    }
+    };
 
-    return <form onSubmit={handleSubmit} className="form-container">
+    return (
+    <form onSubmit={handleSubmit} className="form-container">
         <h1>{name}</h1>
         <input
             className="form-input"
@@ -49,9 +51,11 @@ function Form({ route, method }) {
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
         />
+        {loading && <LoadingIndicator />}
         <button className="form-button" type="submit">
             {name}
         </button>
     </form>
+    );
 }
 export default Form
