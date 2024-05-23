faf78b796e8c40dc0a64b91a97eb3b37b29cdaf4
Bob <bob.vaz@hotmail.fr>
2024-05-23 13:19:46 +0200

    Note init

---
+++ Documents/projects/docblog/frontend/src/components/Notes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Notes.css"
+
+function Note ({ note, onDelete}) {
+    const formattedDated = new Date(note.created_at).toLocaleDateString("en-US")
+
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{formattedDated}</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    );
+}
+
+export default Note
